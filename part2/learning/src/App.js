227a9c9e70031fbca3e4269778eb5c547e9fd5d7
Aldi Gunawan <aldigunawan458@gmail.com>
2021-12-09 13:27:56 +0700

    add partial part2-b

---
+++ part2/learning/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  // state notes
+  const [notes, setNotes] = useState(props.notes);
+  // state new notes
+  const [newNote, setNewNote] = useState("a new note...");
+  // state show only important
+  
+  // event new notes
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    };
+    setNotes(notes.concat(noteObject));
+  };
+  // event handle input/change note
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
