c08fa2158aa2bd5aff0a2717933041a21d96f97c
Aldi Gunawan <aldigunawan458@gmail.com>
2022-01-05 11:26:20 +0700

    uibuild

--- part3/exercises/part3-a/index.js
+++ part3/exercises/part3-a/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.static("build"));
 morgan.token("person", (req, res) => JSON.stringify(req.body));
 
 app.use(express.json());
