80bd2855a54377b66822a11779e585a72b92dd4b
Aldi Gunawan <aldigunawan458@gmail.com>
2022-02-18 16:24:24 +0700

    add part6-a

---
+++ part6/exercises/part6-a/anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = (props) => {
+  const anecdotes = useSelector((state) => state);
+  console.log(anecdotes);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    console.log('vote', id);
+    dispatch(voteAnecdote(id));
+  };
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
